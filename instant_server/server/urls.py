f384b1afb93adf22e7125d5634354a5f969b159a
romainf <romain.faugeroux@gmail.com>
2014-05-29 17:10:37 +0200

    Rolled back to before ios notifs

--- instant_server/server/urls.py
+++ instant_server/server/urls.py
@@ -7,7 +7,7 @@ from mongoengine.queryset import DoesNotExist
 from mongoengine import ValidationError
 from gcm import GCM
 from gcm.gcm import GCMException
-from apnsclient import Message, APNs
+#from apnsclient import Message, APNs
 
 GCM_API_KEY = "AIzaSyCWn_dNhBHFITuVAOAG2r_KDlV5KROg-Oo"
 passphrase = "NapoleonGrouchy"
@@ -40,7 +40,7 @@ def send():
             except GCMException, e:
                 print e
 
-        if user.os=="ios" and True:
+        if user.os=="ios" and False:
             con = Session.new_connection("push_sandbox", cert_file="ck.pem", passphrase=passphrase)
             message = Message([user.reg_id], alert="Gauthier, tu as un nouveau message !", badge=1)
             srv = APNs(con)
